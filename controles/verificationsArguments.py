58d4987724ce18ff4b4d797496fc05675f79ab5e
Kitsune <kitsune@kitsuBian>
2015-04-29 17:28:47 +0200

    Creation fichier docs

--- controles/verificationsArguments.py
+++ controles/verificationsArguments.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import logging
 import Initialisations.loggingConf
-from Initialisations.argument import argumentsParser
 import Initialisations.argument
+
+from Initialisations.argument import argumentsParser
 from controles.recuperationDonnees import verificationChoisi
 
 
 #Liste des arguments du programme
-Attributs = ['g','ar','sg','alb','t','r']
+#Attributs = ['g','ar','sg','alb','t','r']
 
 '''
 Created on 8 oct. 2014
@@ -15,19 +16,29 @@ Verification des arguments saisies par l'utilisateur
 @author: kitsune, Fx
 '''
 
-
-def VerifInt (quantity):
+def convertionInt (quantity):
     '''
-    Verifie si la quantite saisie pour un argument est un entier naturel
-    @param quantity: nombre saisie par l'utilisateur pour un argument
+    Verifie si la quantite saisie pour un argument est un entier naturel.
+    @param quantity: nombre saisie par l'utilisateur pour un argument.
+    @return la valeur en entier naturel.
     '''
 
     #On essais de convertir le format de la saisie en entier
     try:
         #Convertion de la saisie
-        goodQte = int(quantity)
-        logging.info("Un entier a bien ete saisie.")
-
+        if int(quantity):
+            logging.info("Un entier a bien ete saisie.")
+            goodQte = int(quantity)
+        else :
+            logging.info("La saisie n'a pas pu etre convertie en entier.")
+            
+    except ValueError:
+        logging.error("Erreur de conversion,la saisie est une chaine.")
+        print("Il y a une erreur, veuillez saisir un entier naturel.")
+        exit(1)
+        
+    #On va verifier que l'entier saisie est correcte  
+    try:    
         #Si la quantite saisie est un entier naturel
         if goodQte > 0:
             logging.info("L'entier saisie est bien positif.")
@@ -43,17 +54,157 @@ def VerifInt (quantity):
 
             #Dans le dernier cas (strictement inferieur a 0)
         else:
-                #On convertie le negatif en positif
-                goodQte = abs(goodQte)
-                logging.info("L'entier saisie est negatif, la saisie a ete transformer donc en entier positif.")
-                return goodQte
-                #On retroune maintenant un entier naturel
+            #On convertie le negatif en positif
+            goodQte = abs(goodQte)
+            logging.info("L'entier saisie est negatif, la saisie a ete transformer donc en entier positif.")
+            return goodQte
+
     except ValueError:
         #Si le programme n'a pas reussi a convertir la saisie en entier (il s'agit donc de caracteres ou caracteres speciaux
-        logging.error("Erreur de conversion,la saisie est une chaine.")
+        logging.error("Erreur lors de la verification de l'entier.")
         print("Il y a une erreur, veuillez saisir un entier naturel.")
-        exit(1)
+        exit(3)
 
+
+
+
+def pourcentages (pourcentage):
+    '''
+    Fonction qui permet de calculer un taux de remise a l'echelle selon la pourcentage total qui lui est transmis
+    @param pourcentage : total des quantites voulue pour la playlist
+    @return: un decimal qui represente le taux de remise a l'echelle a appliquer a toute les quantites
+    '''
+    
+    #On regarde si le pourcentage est different de 100 donc non correcte
+    if pourcentage != 100:
+
+        #S'il est superieur a 100
+        if pourcentage > 100:
+            
+            #On va mettre tout les pourcentages des arguments proportionnellement pour atteindre les 100
+            proportion = 100 / pourcentage
+
+            #S'il est inferieur a 100
+        else:
+            #On va mettre tout les pourcentges des arguments proportionnellement pour atteindre les 100.
+            proportion = 100 / pourcentage
+            
+        logging.info("Le programme a mis les pourcentages proportionnel a leur nombre car le pourcentage total n'etait pas de 100.")
+    
+    else:
+        proportion = 0
+        
+    
+    return proportion
+
+def conversionMinutes(ArgumentEntier):
+    '''
+    fonction qui permet de convertir un entier (pourcentage d'un argument voulue) en minutes
+    @param ArgumentEntier: pourcentage d'un argument en entier naturel (donc deja verifie) 
+    @return: un entier , le pourcentage est convertie en minutes.
+    '''
+    #On convertie le pourcentage en minutes selon la duree de la playlist
+    Conversion = int (argumentsParser.duree_playlist*ArgumentEntier/100)
+    return Conversion
+
+def remettreEchelle(ListeArg, echelle):
+   
+    '''
+    Selon le pourcentage va remettre a l'echelle toute les valeurs des arguments
+    @param ListeArg : liste des arguments saisie par l'utilisateur
+    @param echelle: proporsition a applique a l'ensemble des entiers de la liste
+    @return: la liste avec les entiers remit a la meme echelle 
+    '''
+    
+    #Pour chaque élements de la liste on fait une mise a l'echelle de la quantite
+    for arg in ListeArg:
+        #mise a l'echelle
+        arg[1] = round(arg[1] * echelle)
+        #conversion en minutes
+        arg[1] = conversionMinutes(arg[1])
+         
+    return ListeArg
+
+def verifArgument():
+    '''
+    Fonction qui va vérifié la saisie des arguments de la ligne de commande
+    @return une liste de liste 
+    '''
+    #On va vérifié s"il y a des arguments optionnel utilisés a partir de ceux qui sont possible
+    
+    #Liste des arguments du programme
+    Attributs = ['g','ar','sg','alb','t']
+    
+    #Initialisation de la playlist
+    ListeArg = ""
+    
+    #On initialise le pourcentage total de la playlist
+    pourcentageTotal = 0
+    
+    #Boucle pour parcourir la liste des arguments saisies par l'utilisateur
+    for arg in Attributs:       
+        i = 0 #On initialise un compteur de tuple par argument
+
+        #On regarde si dans l'argumentParser on trouve un argument qui n'est pas vide (donc ecrit dans la ligne de commande
+        if getattr(argumentsParser, arg) is not None:
+
+            #Si le trouve on le range dans une variable de type liste de liste (liste de tuple d'argument)
+            ListeArg  = getattr(argumentsParser, arg)
+            #On enregistre dans le fichier logging l'information
+            logging.info("L'option "+arg+" est bien present.")
+    
+            #Tant qu'il y a plusieurs des tuples pour un argument trouve
+            while i < len(ListeArg):
+                
+                #On controle la saisie qui represente le pourcentage du tuple
+                try:
+                    #On va donner la saisie a une fonction pour la verifier'''
+                    argVerif = convertionInt(ListeArg[i][1]) #Exemple: on prend la valeur 34 de ("Rock", 34)
+                    
+                    #On rentre la saisie dans une variable qui totalise les pourcentages saisies
+                    pourcentageTotal += argVerif
+                    
+                    #On remplace la saisir de l'utilisateur par un entier correcte s'il ne l'est pas deja
+                    if (ListeArg[i][1] != 100):                       
+                        ListeArg[i][1] = argVerif
+                    
+                    i = i+1 #Incrementation du compteur de tuple
+    
+                except Exception:
+                    logging.error("La fonction de verification d'un entier n'a pas fonctionner")
+            
+                #On regarde si la saisie est present dans la base de donne'''
+                try:
+                    #On controle la valeur voulu pour un argument (Ex pour g : (Rock, 34) on recupere Rock)
+                    #On verifie que le choisie saisie existe dans la base de donnees grace a une fonction
+                    trouveBase = verificationChoisi(ListeArg[i][0], arg) 
+    
+                    #On regarde si on a trouve l'argument dans la base de donnees
+                    if (trouveBase == False):
+                        logging.error("La demande "+ ListeArg[i][0] + " pour l'argument "+ arg +" n'ai pas dans la base.")
+                        #Si c'est faux on affiche un message d'erreur a l'utilisateur
+                        print("Votre demande "+ ListeArg[i][0] + " pour l'argument "+ arg +" n'ai pas dans la base.")
+                        #On quitte le programme
+                        exit(4)
+                            
+                except Exception:
+                    logging.error("La verification de la saisie dans la base de donnée n'a pas pu se faire.")
+
+        #Si l'argument n'a pas ete trouve dans l'argumentParser
+        else:
+            logging.info("L'option "+ arg +" n'est pas presente.")
+     
+    #On va faire une remise a l'echelle selon le pourcentage 
+    # roud() permet de faire un arrondie
+    # pourcentages est une fonction qui va retourner une proportion a appliqué a la remise a l'echelle
+    try :
+        ListeArg = remettreEchelle(ListeArg, round(pourcentages(pourcentageTotal),2))
+        logging.info("Mise a l'echelle reussie.")
+        
+    except Exception :
+        logging.error("Erreur dans la mise l'echelle")
+    
+    return ListeArg  
 
 def Veriff (Attributs):
     '''
@@ -62,7 +213,7 @@ def Veriff (Attributs):
     @return: un boolean trouveArg qui permet de savoir si on a trouve au moin un argument optionnel saisie par l'utilisateur
     '''
 
-    #On initialise le pourcentage total de la playlist#
+    #On initialise le pourcentage total de la playlist
     pourcentage = 0
 
     #On initialise une variable intialiser a false mais qui passe a true si on trouve au moin un argument optionnel dans la ligne de commande
@@ -95,7 +246,7 @@ def Veriff (Attributs):
                 #On controle la saisie qui represente le pourcentage du tuple
                 try:
                     #On va donner la saisie a une fonction pour la verifier'''
-                    argVerif = VerifInt(ArgumentEntier)
+                    argVerif = convertionInt(ArgumentEntier)
 
                     #On rentre la saisie dans une variable qui totalise les pourcentages saisies
                     pourcentage += argVerif
@@ -173,41 +324,4 @@ def Veriff (Attributs):
                             exit(4)
                         #On incremente le i
                         i=i+1
-    return trouveArg
-
-def pourcentages (pourcentage):
-    '''
-    Fonction qui permet de calculer un taux de remise a l'echelle selon la pourcentage total qui lui est transmis
-    @param pourcentage : total des quantites voulue pour la playlist
-    @return: un decimal qui represente le taux de remise a l'echelle a appliquer a toute les quantites
-    '''
-    
-    #On regarde si le pourcentage est different de 100 donc non correcte
-    if pourcentage != 100:
-
-        #S'il est superieur a 100
-        if pourcentage > 100:
-            logging.info("Le programme a mis les pourcentages proportionnel a leur nombre car le max a etait atteint.")
-
-            #On va mettre tout les pourcentages des arguments proportionnellement pour atteindre les 100
-            proportion = 100 / pourcentage
-
-            #S'il est inferieur a 100
-        else:
-            #On va mettre tout les pourcentges des arguments proportionnellement pour atteindre les 100.
-            proportion = 100 / pourcentage
-
-        return proportion
-    else:
-        proportion = 0
-        return proportion
-
-def conversionMinutes(ArgumentEntier):
-    '''
-    fonction qui permet de convertir un entier (pourcentage d'un argument voulue) en minutes
-    @param ArgumentEntier: pourcentage d'un argument en entier naturel (donc deja verifie) 
-    @return: un entier , le pourcentage est convertie en minutes.
-    '''
-    #On convertie le pourcentage en minutes selon la duree de la playlist
-    Conversion = int (argumentsParser.duree_playlist*ArgumentEntier/100)
-    return Conversion
+    return trouveArg             
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
+import controles.playlistFormat
+import random
 from controles.verificationsArguments import Veriff
+from controles.verificationsArguments import verifArgument
 from controles.recuperationDonnees import recuperationDonnees
 from Initialisations.argument import argumentsParser
-import controles.playlistFormat
-from controles.playlistFormat import writeM3U, writeXSPF
-import random
-
+from controles.playlistFormat import writeM3U, writeXSPF, writePLS
 
 Attributs=['g','ar','sg','alb','t']
 #Liste des arguments du programme
 
+#on va recupere la liste des arguments correcte
+ListeArg = verifArgument()
 
 recupArg = Veriff(Attributs)
 #On execute la requete qui va lancer le controle des saisies (argument et entier) de l'utilisateur
@@ -19,7 +21,7 @@ playlist = recuperationDonnees(argumentsParser,recupArg)
 #On recupere la playlist des morceaux trier selon les arguments demander par l'utilisateur
 
 random.shuffle(playlist)
-#On mélange la playlist
+#On melange la playlist
 
 if (argumentsParser.type_playlist =='m3u'):
     writeM3U(argumentsParser, playlist)
@@ -30,5 +32,5 @@ if(argumentsParser.type_playlist=='xspf'):
     print('La playlist a bien ete genere')
 
 if(argumentsParser.type_playlist=='pls'):
-    writeXSPF(argumentsParser, playlist)
+    writePLS(argumentsParser, playlist)
     print('La playlist a bien ete genere')
