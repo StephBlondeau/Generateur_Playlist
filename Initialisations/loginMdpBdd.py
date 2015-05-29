db3bb3a25591d9f009ddf162b8a0a7dfd88f593b
Kitsune <kitsune@kitsuBian>
2015-05-29 11:05:59 +0200

    Ajout login

--- Initialisations/loginMdpBdd.py
+++ Initialisations/loginMdpBdd.py
@@ -1,3 +1,6 @@
+'''
+Login et Mdp de la base de donnees postgreSql
+'''
 #Login et mdp de la base de données postgreSql
-loginBdd = 'monlogin'
-mdpBdd = 'mon mdp'
+loginBdd = 's.blondeau'
+mdpBdd = 'michelle3737'
